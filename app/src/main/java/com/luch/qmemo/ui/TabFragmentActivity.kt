be623149a5eb89f7b96cbb00f9f8397395beca05
luhang <15333805663@sina.cn>
2018-01-04 18:05:55 +0800

    主界面调整

--- app/src/main/java/com/luch/qmemo/ui/TabFragmentActivity.kt
+++ app/src/main/java/com/luch/qmemo/ui/TabFragmentActivity.kt
@@ -4,9 +4,16 @@ import android.app.Fragment
 import android.app.FragmentManager
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.widget.LinearLayout
 import com.luch.qmemo.R
 import com.luch.qmemo.ui.fragment.IndexFragment
 import kotlinx.android.synthetic.main.activity_tabfragment.*
+import android.support.annotation.NonNull
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
+import android.support.v4.app.FragmentTransaction
+import android.view.MenuItem
+
 
 /**
  * Creator lh on 2017/6/30 10:22.
@@ -19,40 +26,42 @@ class TabFragmentActivity : FragmentActivity() {
     private var fm: FragmentManager? = this.fragmentManager
     private var index :IndexFragment? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabfragment)
-        init()
+        nav_bottom.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-    private fun init() {
 
-        nav_bottom.setOnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.item_tab_one -> {
-                    if (index ==null){
-                        index = IndexFragment()
-                    }
-                    fm.apply { index }
-                    fm.run { index }
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.item_tab_two -> {
 
-                    return@setOnNavigationItemSelectedListener true
+    private val mOnNavigationItemSelectedListener
+            = OnNavigationItemSelectedListener { item ->
+        val ft = supportFragmentManager.beginTransaction()
+        when (item.itemId) {
+            R.id.item_tab_one -> {
+                if (index ==null){
+                    index = IndexFragment()
+                    ft.add(R.id.lin_content,index)
                 }
-                R.id.item_tab_three -> {
 
-                    return@setOnNavigationItemSelectedListener true
-                }
-                else -> {
-                    false
-                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.item_tab_two -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.item_tab_three -> {
+                return@OnNavigationItemSelectedListener true
             }
         }
-
+        false
     }
 
 }
+
+
+
+
+
 
 
--- app/src/main/java/com/luch/qmemo/ui/fragment/IndexFragment.kt
+++ app/src/main/java/com/luch/qmemo/ui/fragment/IndexFragment.kt
@@ -1,28 +1,28 @@
 package com.luch.qmemo.ui.fragment
 
+import android.app.Fragment
+import com.luch.qmemo.BaseFragment
 import com.luch.qmemo.R
+import com.luch.qmemo.R.id.bar_title
 import com.luch.qmemo.base.BaseActivity
 import kotlinx.android.synthetic.main.fragment_index.*
 
 /**
  * Creator lh on 2017/9/11 16:02.
  * Email:3021634343@qq.com
- * Description:
+ * Description: 首页
  */
 
-class IndexFragment : BaseActivity() {
-    override fun bundLayout(): Int {
+class IndexFragment : BaseFragment() {
+    override fun attachLayoutRes(): Int {
         return R.layout.fragment_index
-
     }
 
-    override fun initView() {
-        bar_title.setTitle("首页资讯")
-
-
+    override fun initViews() {
     }
 
-    override fun initData() {
+    override fun updateViews(isRefresh: Boolean) {
     }
+
 
 }
